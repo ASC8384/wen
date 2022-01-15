a1b2252356a24d801b24eb4b2f17ae9020a54da1
ASC_8384 <ASC_8384@foxmail.com>
2022-01-15 20:52:10 +0800

    Add: 编译器

---
+++ wen/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/ASC8384/wen/src/compiler"
+)
+
+func main() {
+
+	// read file
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Printf("Usage: %s filename\n", args[0])
+		return
+	}
+	filename := args[1]
+	code, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %s\n", filename)
+		return
+	}
+
+	// execute
+	compiler.Execute(string(code), filename)
+
+}
