044e6bd1f74e358d8bfe46804ec3b7002b8fd68f
ASC_8384 <ASC_8384@foxmail.com>
2023-12-19 17:19:51 +0800

    Add: C backend for ilovebuaa

---
+++ src/compiler/c.go
@@ -0,0 +1,88 @@
+package compiler
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+)
+
+func GetCCode(source []byte) bytes.Buffer {
+	var buff bytes.Buffer
+	var programStart = `
+	extern int putchar(int);
+	extern char getchar();
+
+	char memory[30000];
+	int ptr = 0;
+
+	int main () {
+	`
+	buff.WriteString(programStart)
+
+	for _, bt := range source {
+		switch bt {
+		case 'i':
+			buff.WriteString("ptr++;\n")
+		case 'a':
+			buff.WriteString("ptr--;\n")
+		case 'l':
+			buff.WriteString("memory[ptr]++;\n")
+		case 'e':
+			buff.WriteString("memory[ptr]--;\n")
+		case 'o':
+			buff.WriteString("putchar(memory[ptr]);\n")
+		case 'v':
+			buff.WriteString("memory[ptr] = getchar();\n")
+		case 'b':
+			buff.WriteString("while (memory[ptr]) {\n")
+		case 'u':
+			buff.WriteString("}\n")
+		}
+	}
+	buff.WriteString("return 0;}")
+
+	return buff
+}
+
+func writeToFile(buffer *bytes.Buffer) (string, error) {
+	// tmpfile, err := os.CreateTemp("", "wen*.c")
+	// if err != nil {
+	// 	return "", err
+	// }
+	err := os.WriteFile("a.c", buffer.Bytes(), 0644)
+	// if _, err := tmpfile.Write(buffer.Bytes()); err != nil {
+	// 	return "", err
+	// }
+	// if err := tmpfile.Close(); err != nil {
+	// 	return "", err
+	// }
+	return "a.c", err
+	// return tmpfile.Name(), nil
+}
+
+func GenerateCCode(source []byte) (string, error) {
+	buff := GetCCode(source)
+	filename, err := writeToFile(&buff)
+	if err != nil {
+		return "", err
+	}
+	return filename, nil
+}
+
+func CompileCCode(filename string) (string, error) {
+	cmd := exec.Command("gcc", "-o", "a.out", filename)
+	if err := cmd.Run(); err != nil {
+		return "", err
+	}
+	return "a.out", nil
+}
+
+func RunCCode(filename string) error {
+	cmd := exec.Command("./a.out")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
--- wen/main.go
+++ wen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -9,20 +10,59 @@ import (
 
 func main() {
 
+	compile := flag.Bool("c", false, "compile")
+	flag.Parse()
+
 	// read file
 	args := os.Args
-	if len(args) != 2 {
+	if len(args) == 2 {
+		filename := args[1]
+		code, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Printf("Error reading file: %s\n", filename)
+			return
+		}
+		// execute
+		compiler.Execute(string(code), filename)
+		return
+	}
+	if len(args) != 3 {
 		fmt.Printf("Usage: %s filename\n", args[0])
 		return
 	}
-	filename := args[1]
+	filename := args[2]
 	code, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Printf("Error reading file: %s\n", filename)
 		return
 	}
-
-	// execute
-	compiler.Execute(string(code), filename)
+	if *compile {
+		var cfilename string
+		cfilename, err = compiler.GenerateCCode(code)
+		compiler.CompileCCode(cfilename)
+		compiler.RunCCode(cfilename)
+		// ccode := GetCCode(code)
+		// cfilename, err := writeToFile(&ccode)
+		// if err != nil {
+		// 	os.Exit(1)
+		// }
+		// output := "./a.out"
+		// gcc := exec.Command("gcc", "-O3", "-Ofast", "-o", output, cfilename)
+		// gcc.Stdout = os.Stdout
+		// gcc.Stderr = os.Stderr
+		// err = gcc.Run()
+		// if err != nil {
+		// 	fmt.Printf("Error launching gcc: %s\n", err)
+		// 	return
+		// }
+		// exe := exec.Command(output)
+		// exe.Stdout = os.Stdout
+		// exe.Stderr = os.Stderr
+		// err = exe.Run()
+		// if err != nil {
+		// 	fmt.Printf("Error launching %s: %s\n", output, err)
+		// 	os.Exit(1)
+		// }
+	}
 
 }
